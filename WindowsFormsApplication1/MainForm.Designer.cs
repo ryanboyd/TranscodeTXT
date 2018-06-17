a7c224c3f14a164a4cd3264a0715da4d1a9f9955
Ryan L. Boyd <ryanb@ryanb.cc>
2018-06-17 09:28:18 -0500

    v1.00
    Initial Upload

---
+++ WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.BgWorker = new System.ComponentModel.BackgroundWorker();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.FolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.FilenameLabel = new System.Windows.Forms.Label();
+            this.InputEncodingDropdown = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ScanSubfolderCheckbox = new System.Windows.Forms.CheckBox();
+            this.OutputEncodingDropdown = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveOutputDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.FileTypeTextbox = new System.Windows.Forms.TextBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BgWorker
+            // 
+            this.BgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorkerClean_DoWork);
+            this.BgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgWorker_RunWorkerCompleted);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(70, 247);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(152, 32);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // FolderBrowser
+            // 
+            this.FolderBrowser.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.FolderBrowser.ShowNewFolderButton = false;
+            // 
+            // FilenameLabel
+            // 
+            this.FilenameLabel.AutoEllipsis = true;
+            this.FilenameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FilenameLabel.Location = new System.Drawing.Point(12, 298);
+            this.FilenameLabel.Name = "FilenameLabel";
+            this.FilenameLabel.Size = new System.Drawing.Size(260, 15);
+            this.FilenameLabel.TabIndex = 6;
+            this.FilenameLabel.Text = "Waiting to process texts...";
+            this.FilenameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // InputEncodingDropdown
+            // 
+            this.InputEncodingDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.InputEncodingDropdown.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputEncodingDropdown.FormattingEnabled = true;
+            this.InputEncodingDropdown.Location = new System.Drawing.Point(6, 53);
+            this.InputEncodingDropdown.Name = "InputEncodingDropdown";
+            this.InputEncodingDropdown.Size = new System.Drawing.Size(248, 23);
+            this.InputEncodingDropdown.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Input File Encoding:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.FileTypeTextbox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.ScanSubfolderCheckbox);
+            this.groupBox2.Controls.Add(this.OutputEncodingDropdown);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.InputEncodingDropdown);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(260, 211);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Encoding Selection";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Input File Type:";
+            // 
+            // ScanSubfolderCheckbox
+            // 
+            this.ScanSubfolderCheckbox.AutoSize = true;
+            this.ScanSubfolderCheckbox.Location = new System.Drawing.Point(128, 165);
+            this.ScanSubfolderCheckbox.Name = "ScanSubfolderCheckbox";
+            this.ScanSubfolderCheckbox.Size = new System.Drawing.Size(126, 20);
+            this.ScanSubfolderCheckbox.TabIndex = 13;
+            this.ScanSubfolderCheckbox.Text = "Scan Subfolders";
+            this.ScanSubfolderCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // OutputEncodingDropdown
+            // 
+            this.OutputEncodingDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.OutputEncodingDropdown.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputEncodingDropdown.FormattingEnabled = true;
+            this.OutputEncodingDropdown.Location = new System.Drawing.Point(6, 106);
+            this.OutputEncodingDropdown.Name = "OutputEncodingDropdown";
+            this.OutputEncodingDropdown.Size = new System.Drawing.Size(248, 23);
+            this.OutputEncodingDropdown.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Output File Encoding:";
+            // 
+            // FileTypeTextbox
+            // 
+            this.FileTypeTextbox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FileTypeTextbox.Location = new System.Drawing.Point(6, 161);
+            this.FileTypeTextbox.Name = "FileTypeTextbox";
+            this.FileTypeTextbox.Size = new System.Drawing.Size(100, 26);
+            this.FileTypeTextbox.TabIndex = 16;
+            this.FileTypeTextbox.Text = "*.txt";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.ClientSize = new System.Drawing.Size(284, 321);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.FilenameLabel);
+            this.Controls.Add(this.StartButton);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 360);
+            this.MinimumSize = new System.Drawing.Size(300, 360);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TranscodeTXT";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker BgWorker;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.FolderBrowserDialog FolderBrowser;
+        private System.Windows.Forms.Label FilenameLabel;
+        private System.Windows.Forms.ComboBox InputEncodingDropdown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.FolderBrowserDialog saveOutputDialog;
+        private System.Windows.Forms.ComboBox OutputEncodingDropdown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox ScanSubfolderCheckbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FileTypeTextbox;
+    }
+}
+
---
+++ WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class MainForm : Form
+    {
+
+
+        //initialize the space for our dictionary data
+        DictionaryData DictData = new DictionaryData();
+
+
+
+        //this is what runs at initialization
+        public MainForm()
+        {
+
+            InitializeComponent();
+
+
+            foreach (var encoding in Encoding.GetEncodings())
+            {
+                InputEncodingDropdown.Items.Add(encoding.Name);
+                OutputEncodingDropdown.Items.Add(encoding.Name);
+            }
+
+            try
+            {
+                InputEncodingDropdown.SelectedIndex = InputEncodingDropdown.FindStringExact(Encoding.Default.BodyName);
+            }
+            catch
+            {
+                InputEncodingDropdown.SelectedIndex = InputEncodingDropdown.FindStringExact("utf-8");
+            }
+
+            try
+            {
+                OutputEncodingDropdown.SelectedIndex = OutputEncodingDropdown.FindStringExact("utf-8");
+            }
+            catch
+            {
+                OutputEncodingDropdown.SelectedIndex = OutputEncodingDropdown.FindStringExact(Encoding.Default.BodyName);
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+
+           
+
+                    FolderBrowser.Description = "Please choose the location of your .txt files to process";
+                    if (FolderBrowser.ShowDialog() != DialogResult.Cancel) {
+
+                        DictData.TextFileFolder = FolderBrowser.SelectedPath.ToString();
+                
+                        if (DictData.TextFileFolder != "")
+                        {
+
+                            saveOutputDialog.Description = "Please choose a folder for your output files";
+                            saveOutputDialog.SelectedPath = DictData.TextFileFolder;
+                            if (saveOutputDialog.ShowDialog() != DialogResult.Cancel) {
+
+
+                                if (FolderBrowser.SelectedPath == saveOutputDialog.SelectedPath)
+                                {
+                                    MessageBox.Show("You can not use the same folder for text input and text output. If you do this, your original data would be overwritten. Please use a different folder for your output location.", "Folder selection error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    return;
+                                }
+
+                                DictData.OutputFileLocation = saveOutputDialog.SelectedPath;
+                                DictData.FileExtension = FileTypeTextbox.Text.Trim();
+
+
+                                if (DictData.OutputFileLocation != "") {
+
+
+
+                                    StartButton.Enabled = false;
+                                    ScanSubfolderCheckbox.Enabled = false;
+                                    InputEncodingDropdown.Enabled = false;
+                                    FileTypeTextbox.Enabled = false;
+                            
+                                    BgWorker.RunWorkerAsync(DictData);
+                                }
+                            }
+                        }
+
+                    }
+
+                
+
+        }
+
+        
+
+
+
+
+        private void BgWorkerClean_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+
+            DictionaryData DictData = (DictionaryData)e.Argument;
+
+
+            //selects the text encoding based on user selection
+            Encoding InputSelectedEncoding = null;
+            Encoding OutputSelectedEncoding = null;
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                InputSelectedEncoding = Encoding.GetEncoding(InputEncodingDropdown.SelectedItem.ToString());
+                OutputSelectedEncoding = Encoding.GetEncoding(OutputEncodingDropdown.SelectedItem.ToString());
+            });
+
+            
+
+            //get the list of files
+            var SearchDepth = SearchOption.TopDirectoryOnly;
+            if (ScanSubfolderCheckbox.Checked)
+            {
+                SearchDepth = SearchOption.AllDirectories;
+            }
+            var files = Directory.EnumerateFiles(DictData.TextFileFolder, DictData.FileExtension, SearchDepth);
+
+
+
+            try {
+
+                //we want to be conservative and limit the number of threads to the number of processors that we have
+                var options = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount};
+                Parallel.ForEach(files, options, (string fileName) =>
+                {
+
+                    //set up our variables to report
+                    string Filename_Clean = Path.GetFileName(fileName);
+
+                    string SubDirStructure = Path.GetDirectoryName(fileName).Replace(DictData.TextFileFolder, "").TrimStart('\\');
+
+
+                    //creates subdirs if they don't exist
+                    string Output_Location = DictData.OutputFileLocation + '\\' + SubDirStructure;
+
+                    if (!Directory.Exists(Output_Location))
+                    {
+                        Directory.CreateDirectory(Output_Location);
+                    }
+
+                    Output_Location = Path.Combine(Output_Location, Path.GetFileName(fileName));
+
+                        //report what we're working on
+                        FilenameLabel.Invoke((MethodInvoker)delegate
+                    {
+                        FilenameLabel.Text = "Processing: " + Filename_Clean;
+                        FilenameLabel.Invalidate();
+                        FilenameLabel.Update();
+                        FilenameLabel.Refresh();
+                        Application.DoEvents();
+
+                    });
+
+
+
+
+
+
+
+
+
+                    // __        __    _ _          ___        _               _   
+                    // \ \      / / __(_) |_ ___   / _ \ _   _| |_ _ __  _   _| |_ 
+                    //  \ \ /\ / / '__| | __/ _ \ | | | | | | | __| '_ \| | | | __|
+                    //   \ V  V /| |  | | ||  __/ | |_| | |_| | |_| |_) | |_| | |_ 
+                    //    \_/\_/ |_|  |_|\__\___|  \___/ \__,_|\__| .__/ \__,_|\__|
+                    //                                            |_|              
+
+
+                    using (StreamReader inputfile = new StreamReader(fileName, InputSelectedEncoding))
+                    {
+
+                        string readText = inputfile.ReadToEnd();
+                    
+                        //open up the output file
+                        using (StreamWriter outputFile = new StreamWriter(new FileStream(Output_Location, FileMode.Create), OutputSelectedEncoding))
+                        {
+                            outputFile.Write(readText);
+                        }
+                    }
+
+
+                });
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("TranscodeTXT encountered an issue somewhere while trying to analyze your texts. The most common cause of this is trying to open your output file(s) while the program is still running. Did any of your input files move, or is your output file being opened/modified by another application?", "Error while transcoding", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            
+        }
+
+
+        //when the bgworker is done running, we want to re-enable user controls and let them know that it's finished
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            StartButton.Enabled = true;
+            ScanSubfolderCheckbox.Enabled = true;
+            InputEncodingDropdown.Enabled = true;
+            FileTypeTextbox.Enabled = true;
+            FilenameLabel.Text = "Finished!";
+            MessageBox.Show("TranscodeTXT has finished processing your texts.", "Transcode Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+
+
+
+
+        public class DictionaryData
+        {
+
+            public string TextFileFolder { get; set; }
+            public string OutputFileLocation { get; set; }
+            public string FileExtension { get; set; }
+
+        }
+
+
+    }
+    
+
+
+}
